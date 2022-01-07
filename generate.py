5712e9287c87216a4dacfff7403fff0683f47b46
yuhki50 <yuhki50@gmail.com>
2022-01-07 13:33:32 +0900

    Migrate KiCad6

--- generate.py
+++ generate.py
@@ -7,12 +7,12 @@ from pathlib import Path
 try:
     script_dir = Path(__file__).parent.resolve()
 
-    with (script_dir / "sym-lib-table").open("w") as f:
+    with (script_dir / "sym-lib-table").open("w", newline="\n") as f:
         f.write("\n".join([
             "(sym_lib_table",
             "\n".join([
-                f"""  (lib (name {v.stem})(type Legacy)(uri ${{KIPRJMOD}}/library/{v.name})(options "")(descr ""))"""
-                for v in sorted(script_dir.glob("*.lib"), key=lambda v: v.stem)
+                f"""  (lib (name {v.stem})(type KiCad)(uri ${{KIPRJMOD}}/library/{v.name})(options "")(descr ""))"""
+                for v in sorted(script_dir.glob("*.kicad_sym"), key=lambda v: v.stem)
             ]),
             ")",
         ]))
